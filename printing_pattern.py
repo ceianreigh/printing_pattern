7f373cbf3d5af23277806956f8885f470bc1e21e
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-21 22:19:47 +0800

    Add instruction and pseudocode that prints a specific pattern of numbers

---
+++ printing_pattern.py
@@ -0,0 +1,11 @@
+# Print a specific pattern of numbers
+
+# pseudocode
+
+# add the rows of pattern
+
+# create a for loop to iterate through the rows
+
+# create a nested for loop to iterate through the columns
+
+# print the pattern
